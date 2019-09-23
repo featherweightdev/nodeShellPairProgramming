2ded17ab40fcdcf61d7cfa9f3ccef6fc5a70cdab
Featherweight <vais.emma@gmail.com>
2019-09-23 12:37:41 -0400

    installed nodemon

--- bash.js
+++ bash.js
@@ -3,7 +3,7 @@ process.stdout.write("prompt> ");
 process.stdin.on("data", (data) => {
   const cmd = data.toString().trim();
   if (cmd === "pwd") {
-    process.stdout.write(__dirname);
+    process.stdout.write(process.cwd());
 
   }
   process.stdout.write("\nprompt> ");
