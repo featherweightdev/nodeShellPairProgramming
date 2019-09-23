1cff6d3295c3841c8884bb50a8736c02f946d1c5
Featherweight <vais.emma@gmail.com>
2019-09-23 12:34:39 -0400

    pwd command added

--- bash.js
+++ bash.js
@@ -1,8 +1,11 @@
-process.stdout.write("prompt>");
+process.stdout.write("prompt> ");
 
 process.stdin.on("data", (data) => {
   const cmd = data.toString().trim();
+  if (cmd === "pwd") {
+    process.stdout.write(__dirname);
 
-  process.stdout.write("you typed: " + cmd);
+  }
   process.stdout.write("\nprompt> ");
 });
+
